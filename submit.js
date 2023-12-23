a48714a75febf5f1f6fece0bce7bebb5c64d3ebd
catienice <149631703+catienice@users.noreply.github.com>
2023-12-23 11:12:02 -0700

    Update submit.js

--- submit.js
+++ submit.js
@@ -52,11 +52,11 @@ const removeParenth = function(str) {
   };
 
 const scoreScrabble = function(str) {
-  let alphabet = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t:1, d: 2, g: 2,b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }
-  let sum = 0
-
-  for (let index = 0; i  < str.length; i++) {
-    sum += str[i];
+  let alphabet = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t:1, d: 2, g: 2,b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 };
+  let total = 0
+  str = str.toLowerCase();
+  for (let i = 0; i  < str.length; i++) {
+    total += alphabet[str[i]] || 0;
   }
-return sum;
+return total;
   };
